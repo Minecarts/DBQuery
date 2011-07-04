e7ac78019ec7985e51b95ca8e037f3f3705f333d
Stephen <stephen@jazer.com>
2011-07-03 20:53:49 -0400

    Initial commit

---
+++ src/com/minecarts/dbquery/DBQuery.java
@@ -0,0 +1,23 @@
+package com.minecarts.dbquery;
+
+import java.util.logging.Logger;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.util.config.Configuration;
+
+import com.minecarts.dbconnector.providers.*;
+
+public class DBQuery extends org.bukkit.plugin.java.JavaPlugin{
+    public final Logger log = Logger.getLogger("com.minecarts.dbquery"); 
+
+    public void onEnable() {
+        
+    }
+    
+    public void onDisable(){
+    }
+}
