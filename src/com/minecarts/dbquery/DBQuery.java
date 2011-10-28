8c230f4ad8b5c83525c3a57970fa571569c5cf30
Kevin Ingersoll <kingersoll@gmail.com>
2011-10-27 19:39:23 -0700

    Added the other async methods

--- src/com/minecarts/dbquery/DBQuery.java
+++ src/com/minecarts/dbquery/DBQuery.java
@@ -74,6 +74,18 @@ public class DBQuery extends org.bukkit.plugin.java.JavaPlugin {
             super(provider);
         }
         
+        
+        public ArrayList<HashMap> fetch(String sql, Object... params) {
+            if(params.length > 0 && params[params.length - 1] instanceof Callback) {
+                fetch(sql, Arrays.copyOf(params, params.length - 1), (Callback) params[params.length - 1]);
+            }
+            else {
+                fetch(sql, params, null);
+            }
+            
+            return null;
+        }
+        
         public void fetch(final String sql, final Object[] params, final Callback callback) {
             getServer().getScheduler().scheduleAsyncDelayedTask(DBQuery.this, new Runnable() {
                 public void run() {
@@ -92,15 +104,94 @@ public class DBQuery extends org.bukkit.plugin.java.JavaPlugin {
             });
         }
         
-        public ArrayList<HashMap> fetch(String sql, Object... params) {
-            if(params[params.length - 1] instanceof Callback) {
-                fetch(sql, Arrays.copyOf(params, params.length - 1), (Callback) params[params.length - 1]);
+        
+        public ArrayList<HashMap> generatedKeys(String sql, Object... params) {
+            if(params.length > 0 && params[params.length - 1] instanceof Callback) {
+                generatedKeys(sql, Arrays.copyOf(params, params.length - 1), (Callback) params[params.length - 1]);
             }
             else {
-                fetch(sql, params, null);
+                generatedKeys(sql, params, null);
             }
             
             return null;
+        }
+        
+        public void generatedKeys(final String sql, final Object[] params, final Callback callback) {
+            getServer().getScheduler().scheduleAsyncDelayedTask(DBQuery.this, new Runnable() {
+                public void run() {
+                    Object result;
+                    try {
+                        result = AsyncQueryHelper.super.generatedKeys(sql, params);
+                    }
+                    catch(final Exception e) {
+                        result = e;
+                    }
+                    
+                    if(callback != null) {
+                        getServer().getScheduler().scheduleSyncDelayedTask(DBQuery.this, callback.clone(result));
+                    }
+                }
+            });
+        }
+        
+        
+        public Integer affected(String sql, Object... params) {
+            if(params.length > 0 && params[params.length - 1] instanceof Callback) {
+                affected(sql, Arrays.copyOf(params, params.length - 1), (Callback) params[params.length - 1]);
+            }
+            else {
+                affected(sql, params, null);
+            }
+            
+            return null;
+        }
+        
+        public void affected(final String sql, final Object[] params, final Callback callback) {
+            getServer().getScheduler().scheduleAsyncDelayedTask(DBQuery.this, new Runnable() {
+                public void run() {
+                    Object result;
+                    try {
+                        result = AsyncQueryHelper.super.affected(sql, params);
+                    }
+                    catch(final Exception e) {
+                        result = e;
+                    }
+                    
+                    if(callback != null) {
+                        getServer().getScheduler().scheduleSyncDelayedTask(DBQuery.this, callback.clone(result));
+                    }
+                }
+            });
+        }
+        
+        
+        public Integer insertId(String sql, Object... params) {
+            if(params.length > 0 && params[params.length - 1] instanceof Callback) {
+                insertId(sql, Arrays.copyOf(params, params.length - 1), (Callback) params[params.length - 1]);
+            }
+            else {
+                insertId(sql, params, null);
+            }
+            
+            return null;
+        }
+        
+        public void insertId(final String sql, final Object[] params, final Callback callback) {
+            getServer().getScheduler().scheduleAsyncDelayedTask(DBQuery.this, new Runnable() {
+                public void run() {
+                    Object result;
+                    try {
+                        result = AsyncQueryHelper.super.insertId(sql, params);
+                    }
+                    catch(final Exception e) {
+                        result = e;
+                    }
+                    
+                    if(callback != null) {
+                        getServer().getScheduler().scheduleSyncDelayedTask(DBQuery.this, callback.clone(result));
+                    }
+                }
+            });
         }
         
     }
